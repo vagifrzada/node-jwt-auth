915853d104adc270e5fbdd710970dbebe347daa7
Vagif Rufullazada <info@vagif.dev>
2024-01-23 22:57:55 +0400

    Removed ssl support

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,12 @@
-import fs from 'fs'
-import path from 'path'
 import app from './app'
-import https from 'https'
+import http from 'http'
 import { connectToMongo } from './core/database'
 
 const port = process.env.APP_PORT
-const CERT_PATH = path.join(process.cwd(), 'cert')
 
 async function main() {
     await connectToMongo()
-    const server = https.createServer(
-        {
-            key: fs.readFileSync(path.join(CERT_PATH, 'key.pem')),
-            cert: fs.readFileSync(path.join(CERT_PATH, 'cert.pem')),
-        },
-        app
-    )
+    const server = http.createServer(app)
     server.listen(port, () => console.log(`App is running on port ${port}`))
 }
 
